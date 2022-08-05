c47f755f1ada0a0f7b37f8e1e55e2fdeddf0c96e
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-05 23:51:39 +0000

    size done... frontend done

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from tkinter import *
 
 # ------------------------------------------------ CONSTANTS -------------------------------------------------------
 ash = '#4B5D67'
+green = '#3CCF4E'
+red = '#FF1E00'
 
 # ------------------------------------------------ UI SETUP -------------------------------------------------------
 
@@ -13,28 +15,31 @@ app.resizable(False, False)
 
 
 # title label
-title = Label(text="TIMER", bg=ash, font=("MONTSERRAT", 70, "bold"))
+title = Label(text="TIMER", bg=ash, font=("MONTSERRAT", 50, "bold"))
 title.grid(row=0, column=1)
 
 
 # Start and stop button
-start = Button(text="START", bg=ash, highlightthickness=0, font=("MONTSERRAT", 30, "bold") )
+start = Button(text="START", bg=ash, fg=green, highlightthickness=0, padx=5, pady=5, font=("MONTSERRAT", 20, "bold") )
 start.grid(row=2, column=0)
 
-stop = Button(text="STOP", bg=ash, highlightthickness=0, font=("MONTSERRAT", 30, "bold") )
-stop.grid(row=2, column=2)
+reset = Button(text="RESET", fg=red, padx=5, pady=5,  highlightthickness=0, font=("MONTSERRAT", 20, "bold") )
+reset.grid(row=2, column=2)
 
+# CHECK mark
 
+checkMark = Label(text="✅", bg=ash, fg="BLACK", font=("MONTSERRAT", 30, "bold"))
+checkMark.grid(row=3, column=1)
 
 # creating the canvas
-canvas = Canvas(width=600, height=340, bg=ash, highlightthickness=0)
+canvas = Canvas(width=450, height=300, bg=ash, highlightthickness=0)
 
 # adding an image to the canvas
 background_img = PhotoImage(file="./pck.png")
-canvas.create_image(150, 170, image=background_img)
+canvas.create_image(140, 150, image=background_img)
 
 # adding text to the canvas
-canvas.create_text(460, 170, text="00:00", font=("COURIER", 80, "bold"))
+canvas.create_text(370, 150, text="00:00", font=("COURIER", 50, "bold"))
 canvas.grid(row=1, column=1)
 
 
