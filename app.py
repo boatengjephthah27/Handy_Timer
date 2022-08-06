9291441d814036e8576c71230e95c2368dd8b09d
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-06 00:15:34 +0000

    start button func

--- app.py
+++ app.py
@@ -6,6 +6,29 @@ ash = '#4B5D67'
 green = '#3CCF4E'
 red = '#FF1E00'
 
+
+# ------------------------------------------------ TIMER MECHANISM -------------------------------------------------------
+
+def start_timer():
+    countDown(10)
+
+
+
+
+
+
+# ------------------------------------------------ COUNTDOWN MECHANISM -------------------------------------------------------
+
+def countDown(count):
+    canvas.itemconfig(timer, text=count)
+    if count > 0:
+        app.after(1000, countDown, count-1)
+
+
+
+
+
+
 # ------------------------------------------------ UI SETUP -------------------------------------------------------
 
 app = Tk()
@@ -20,7 +43,7 @@ title.grid(row=0, column=1)
 
 
 # Start and stop button
-start = Button(text="START", bg=ash, fg=green, highlightthickness=0, padx=5, pady=5, font=("MONTSERRAT", 20, "bold") )
+start = Button(text="START", bg=ash, fg=green, highlightthickness=0, padx=5, pady=5, font=("MONTSERRAT", 20, "bold"), command=start_timer)
 start.grid(row=2, column=0)
 
 reset = Button(text="RESET", fg=red, padx=5, pady=5,  highlightthickness=0, font=("MONTSERRAT", 20, "bold") )
@@ -32,17 +55,15 @@ checkMark = Label(text="✅", bg=ash, fg="BLACK", font=("MONTSERRAT", 30, "bold"
 checkMark.grid(row=3, column=1)
 
 # creating the canvas
-canvas = Canvas(width=450, height=300, bg=ash, highlightthickness=0)
+canvas = Canvas(width=460, height=300, bg=ash, highlightthickness=0)
 
 # adding an image to the canvas
 background_img = PhotoImage(file="./pck.png")
 canvas.create_image(140, 150, image=background_img)
 
 # adding text to the canvas
-canvas.create_text(370, 150, text="00:00", font=("COURIER", 50, "bold"))
+timer = canvas.create_text(370, 150, text="00:00", font=("COURIER", 60, "bold"))
 canvas.grid(row=1, column=1)
-
-
 
 
 
